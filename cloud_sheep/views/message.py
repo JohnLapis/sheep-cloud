732704cc35f4614cdcad56533ecc39540381b833
john <borges-2014@live.com>
2020-12-30 15:47:42 -0300

    refactor: Add handler for the JSON-unserializable ObjectId

--- cloud_sheep/views/message.py
+++ cloud_sheep/views/message.py
@@ -1,8 +1,25 @@
+from functools import wraps
+
 from bson.objectid import ObjectId
 from flask import abort, request
 from flask.views import MethodView
 
 from ..entities.message import create_message, create_partial_message
+
+def handle_message(view):
+    def serialize_id(message):
+        message["_id"] = str(message["_id"])
+        return message
+
+    @wraps(view)
+    def wrapper(*args, **kwargs):
+        res = view(*args, **kwargs)
+        if type(res) is list:
+            return list(map(serialize_id, res))
+        else:
+            return serialize_id(res)
+
+    return wrapper
 
 
 class MessageView(MethodView):
@@ -10,14 +27,15 @@ class MessageView(MethodView):
         super()
         self.db = db
 
+    @handle_message
     def get(self, id=None):
         if id is None:
             # request.args
             pass
         else:
             message = self.db.find_one(ObjectId(id))
-            if not message: abort(404)
-            message["_id"] = str(message["_id"])
+            if not message:
+                abort(404)
             return message
 
     def post(self):
