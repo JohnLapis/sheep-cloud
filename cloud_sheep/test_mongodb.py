4e4abf5b6f0fc0acdfbd400caa829b6ff9b92d54
john <borges-2014@live.com>
2021-01-01 02:07:08 -0300

    test: Add tests for mongodb.py

--- cloud_sheep/test_mongodb.py
+++ cloud_sheep/test_mongodb.py
@@ -2,9 +2,124 @@ from datetime import datetime
 
 import pytest
 
-from .exceptions import InvalidValue
-from .mongodb import convert_to_date
+from .exceptions import InvalidValue, InvalidParam, InvalidQuery
+from .mongodb import convert_to_date, DatabaseClient
 
+
+@pytest.fixture
+def app(scope="module"):
+    from flask import Flask
+
+    return Flask(__name__)
+
+
+class TestDatabaseClient:
+    @classmethod
+    def setup_class(cls):
+        cls.client = DatabaseClient()
+
+    @classmethod
+    def teardown_class(cls):
+        cls.client.conn.close()
+
+    @pytest.mark.parametrize(
+        "param,values,expected",
+        [
+            (
+                "created_at",
+                ["lt:2020"],
+                {"created_at": [{"$lt": datetime(year=2020, month=1, day=1)}]},
+            ),
+            (
+                "last_modified",
+                ["gt:202012"],
+                {"last_modified": [{"$gt": datetime(year=2020, month=12, day=1)}]},
+            ),
+            (
+                "created_at",
+                ["lt:20201230", "gt:20201229"],
+                {
+                    "created_at": [
+                        {"$lt": datetime(year=2020, month=12, day=30)},
+                        {"$gt": datetime(year=2020, month=12, day=29)},
+                    ]
+                },
+            ),
+        ],
+    )
+    def test_create_one_param_query_with_valid_input(self, param, values, expected):
+        assert self.client.create_one_param_query(param, values) == expected
+
+    def test_create_one_param_query_with_invalid_input(self):
+        with pytest.raises(InvalidValue):
+            self.client.create_one_param_query("created_at", ["lt:not a date"])
+        with pytest.raises(InvalidParam):
+            self.client.create_one_param_query("invalid param", ["xx:whatever"])
+        with pytest.raises(InvalidValue):
+            self.client.create_one_param_query("last_modified", ["xx:whatever"])
+        with pytest.raises(InvalidValue):
+            self.client.create_one_param_query("created_at", [""])
+
+    @pytest.mark.parametrize(
+        "url_query,expected",
+        [
+            (
+                "/api/messages?created_at=lt:2020",
+                {
+                    "$and": [
+                        {"created_at": [{"$lt": datetime(year=2020, month=1, day=1)}]}
+                    ]
+                },
+            ),
+            (
+                "/api/messages?last_modified=gt:202011",
+                {
+                    "$and": [
+                        {
+                            "last_modified": [
+                                {"$gt": datetime(year=2020, month=11, day=1)}
+                            ]
+                        }
+                    ]
+                },
+            ),
+            (
+                "/api/messages?created_at=gt:20200625&created_at=lt:202011"
+                + "&last_modified=gt:202012",
+                {
+                    "$and": [
+                        {
+                            "created_at": [
+                                {"$gt": datetime(year=2020, month=6, day=25)},
+                                {"$lt": datetime(year=2020, month=11, day=1)},
+                            ],
+                        },
+                        {
+                            "last_modified": [
+                                {"$gt": datetime(year=2020, month=12, day=1)},
+                            ],
+                        },
+                    ],
+                },
+            ),
+        ],
+    )
+    def test_create_query_with_valid_input(self, app, url_query, expected):
+        with app.test_request_context(url_query) as ctx:
+            assert self.client.create_query(ctx.request.args) == expected
+
+    @pytest.mark.parametrize(
+        "url_query",
+        [
+            "/api/messages",
+            "/api/messages?a=",
+            "/api/messages?&=a==",
+        ],
+    )
+    def test_create_db_query_with_invalid_params(self, app, url_query):
+        with app.test_request_context(url_query) as ctx:
+            with pytest.raises((InvalidValue, InvalidQuery)):
+                self.client.create_query(ctx.request.args)
 
 @pytest.mark.parametrize(
     "date,expected",
