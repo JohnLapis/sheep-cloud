ea4ab8137515b0b86a52063a3f7e348954a1c1cb
john <borges-2014@live.com>
2020-12-31 23:55:35 -0300

    feat: Creation of database query from url params

--- cloud_sheep/mongodb.py
+++ cloud_sheep/mongodb.py
@@ -5,6 +5,7 @@ from urllib.parse import quote_plus as encode_url
 
 import mongoengine
 from .exceptions import InvalidValue
+from .utils import get_param_type
 
 
 def convert_to_date(value):
@@ -27,6 +28,14 @@ TYPE_CONVERTER_DICT = {
 def get_type_converter(type):
     return TYPE_CONVERTER_DICT[type]
 
+OPERATOR_TABLE = {
+    "gt": "$gt",
+    "lt": "$lt",
+    "and": "$and",
+}
+
+def get_db_op(op):
+    return OPERATOR_TABLE[op]
 
 def get_db_host():
     with open("db_config.json", "r") as f:
@@ -44,3 +53,25 @@ class DatabaseClient:
 
     def __getattr__(self, name):
         return getattr(self._db, name)
+
+    def create_one_param_query(self, param, values):
+        filters = []
+        for value in values:
+            try:
+                op, value = value.split(":")
+            except ValueError:
+                raise InvalidValue
+            param_type = get_param_type(param)
+            filters.append({
+                get_db_op(op): get_type_converter(param_type)(value)
+            })
+
+        return {param: filters}
+
+    def create_query(self, query_dict):
+        filters = []
+        for param in query_dict.keys():
+            values = query_dict.getlist(param)
+            filters.append(self.create_one_param_query(param, values))
+
+        return {get_db_op("and"): filters}
